a7fbd639c0722d0f56e562b2e488acdd954c8439
Milan Vukasović <92845311+MilanVukasovi@users.noreply.github.com>
2022-10-30 08:26:33 +0100

    Project initialized
    Project is fully setup, application is buildable and ready to go

---
+++ Client/Clique/Application/SceneDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  SceneDelegate.swift
+//  Clique
+//
+//  Created by Infinum on 30.10.2022..
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options
+    connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let storyboard = UIStoryboard(name: "Initial" , bundle:nil)
+        let viewController = storyboard.instantiateInitialViewController()
+        
+        window = UIWindow(windowScene: windowScene)
+        
+        window?.rootViewController = viewController
+        window?.makeKeyAndVisible()
+    }
+
+}
+
---
+++ Client/Clique/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Clique
+//
+//  Created by Infinum on 30.10.2022..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
