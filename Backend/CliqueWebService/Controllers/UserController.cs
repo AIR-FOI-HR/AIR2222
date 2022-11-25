6ce367ba52a0ed81ec87541d35ca075fcbc65b8e
MilanVukasovi <mvukasovi@foi.hr>
2022-11-25 17:01:35 +0100

    Added function UpdateUserData
    Function updates user data based on user id

--- Backend/CliqueWebService/Controllers/UserController.cs
+++ Backend/CliqueWebService/Controllers/UserController.cs
@@ -81,5 +81,57 @@ namespace CliqueWebService.Controllers
                 return StatusCode(StatusCodes.Status500InternalServerError, returnResponse);
             }
         }
+
+        [HttpPost]
+        [Route("UpdateUserData")]
+        public ActionResult UpdateUserData([FromBody] User user)
+        {
+            DocumentResponse docResponse = new DocumentResponse();
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _db.Connect();
+                }
+                catch (Exception ex)
+                {
+                    docResponse.Error = ex.Message;
+                    docResponse.Status = "500 - Internal Server Error";
+                    docResponse.Method = "POST";
+                    return StatusCode(StatusCodes.Status500InternalServerError, docResponse);
+                }
+                string query = $"UPDATE Users SET name = '{user.name}', surname = '{user.surname}', email = '{user.email}', contact_no = '{user.contact_no}', " +
+                    $"birth_data = '{user.birth_data}', gender = {user.gender} WHERE user_id = {user.user_id}";
+                _db.BeginTransaction();
+                try
+                {
+                    _db.ExecuteNonQuery(query);
+                    _db.CommitTransaction();
+                    docResponse.Message = "User successfully updated";
+                    docResponse.Status = "200 - OK";
+                    docResponse.Method = "POST";
+                    return Ok(docResponse);
+                }
+                catch
+                {
+                    _db.RollbackTransaction();
+                    _db.CommitTransaction();
+                    docResponse.Error = "Couldn't update user";
+                    docResponse.Status = "500 - Internal Server Error";
+                    docResponse.Method = "POST";
+                    return StatusCode(StatusCodes.Status500InternalServerError, docResponse);
+                }
+            }
+            else
+            {
+                docResponse.Error = "Incorrectly formated JSON request";
+                docResponse.Status = "400 - Bad Request";
+                docResponse.Method = "POST";
+                return BadRequest(docResponse);
+            }
+        }
+
+
     }
 }
