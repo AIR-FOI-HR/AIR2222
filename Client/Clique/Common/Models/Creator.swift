33c4a76cf67138555115eb5007f1d2a6d7e9a595
David-Viljevac <84078493+David-Viljevac@users.noreply.github.com>
2022-11-29 00:48:02 +0100

    Create Creator struct which is used for json response

---
+++ Client/Clique/Common/Models/Creator.swift
@@ -0,0 +1,25 @@
+//
+//  Creator.swift
+//  Clique
+//
+//  Created by Infinum on 26.11.2022..
+//
+
+import Foundation
+
+struct Creator: Codable {
+    
+    let userID: Int
+    let userName: String
+    let userSurname: String
+    let userEmail: String
+    
+    enum CodingKeys: String, CodingKey{
+        case userID = "user_id"
+        case userName = "name"
+        case userSurname = "surname"
+        case userEmail = "email"
+
+    }
+}
+
