5a6a79bda136e64c7f380bbdb7fc369b2f69d47e
MilanVukasovi <mvukasovi@foi.hr>
2022-11-29 12:18:35 +0100

    Fixed login authentication

--- Backend/CliqueWebService/Helpers/Models/User.cs
+++ Backend/CliqueWebService/Helpers/Models/User.cs
@@ -4,6 +4,7 @@ namespace CliqueWebService.Helpers.Models
 {
     public class User
     {
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public int? user_id { get; set; }
         public string? name { get; set; }
         public string? surname { get; set; }
@@ -12,10 +13,13 @@ namespace CliqueWebService.Helpers.Models
         public string? gender { get; set; }
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public string? password { get; set; }
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public string? contact_no { get; set; }
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public DateTime? birth_data { get; set; }
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public string? profile_pic { get; set; }
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public string? bio { get; set; }
     }
 }
