42f72dad16037e88e53cae08f3c83b128654365d
David-Viljevac <84078493+David-Viljevac@users.noreply.github.com>
2022-11-30 17:50:59 +0100

    Created outlet for filter button and implemented function for searching events list trough text field

--- Client/Clique/Modules/Event list/EventListViewController.swift
+++ Client/Clique/Modules/Event list/EventListViewController.swift
@@ -10,31 +10,36 @@ import Foundation
 import UIKit
 import SwiftUI
 
-class EventListViewController : UIViewController , UIAlertViewDelegate {
+class EventListViewController : UIViewController{
     
     private var datasource: [Event] = []{
         didSet{_tableView.reloadData()}}
+    private var allEvents: [Event] = []
     private var eventServices = EventServices()
-    
     @IBOutlet private var _searchEventsTextField: UITextField!
     @IBOutlet private var _tableView: UITableView!
+    @IBOutlet private var _buttonFilter: UIButton!
+    
     
     override func viewDidLoad(){
         
         super.viewDidLoad()
         _setupUI()
         self._tableView.reloadData()
-        
         _searchEventsTextField.layer.shadowOpacity = 0.3
         _searchEventsTextField.layer.shadowRadius = 2.0
         _searchEventsTextField.layer.shadowOffset = CGSizeMake(3, 3)
         _searchEventsTextField.layer.shadowColor = UIColor.gray.cgColor
+        
     }
+    
+    
 }
 
 private extension EventListViewController {
     func _setupUI(){
         getEvents()
+        _searchEventsTextField.delegate = self
         _tableView.dataSource = self
         _tableView.delegate = self
         _tableView.register(UINib(nibName: "EventListTableViewCell", bundle: nil), forCellReuseIdentifier: "EventListTableViewCell")
@@ -47,13 +52,32 @@ private extension EventListViewController {
             case .success(let events):
                 events.forEach{event in
                     self.datasource.append(event)
-                    
                 }
+                self.allEvents = self.datasource
             case .failure(let error):
                 print(error)
             }
         }
     }
+    
+    func getSearchResults(){
+        guard let searchText = _searchEventsTextField.text
+        else{
+            return
+        }
+        if(searchText != ""){
+            datasource = allEvents.filter{ event in
+                return event.eventName.lowercased().contains(searchText.lowercased())
+            }
+            
+        }else{
+            datasource = allEvents
+        }
+        
+        debugPrint(datasource)
+        _tableView.reloadData()
+    }
+    
 }
 extension EventListViewController : UITableViewDelegate{
     
@@ -81,4 +105,14 @@ extension EventListViewController : UITableViewDataSource{
 
         return cell
     }
+}
+
+extension EventListViewController : UITextFieldDelegate{
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == _searchEventsTextField{
+            getSearchResults()
+        }
+        return true
+    }
+    
 }
