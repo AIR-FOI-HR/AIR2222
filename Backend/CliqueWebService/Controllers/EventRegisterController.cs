a9786c1227d089b99d1b0ef0a2cd01202cb78786
MilanVukasovi <mvukasovi@foi.hr>
2022-12-08 09:38:05 +0100

    Added EventRegisterController
    Added EventRegisterController and function to check if user is signed on event

---
+++ Backend/CliqueWebService/Controllers/EventRegisterController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DataAccess;
+using CliqueWebService.Helpers;
+using CliqueWebService.Helpers.Models;
+
+
+namespace CliqueWebService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventRegisterController : ControllerBase
+    {
+        Database _db;
+        BusinessLogic _businessLogic;
+        private readonly IConfiguration _configuration;
+
+        public EventRegisterController(IConfiguration configuration)
+        {
+            _configuration = configuration ?? throw new ArgumentNullException();
+            _db = new Database(configuration.GetConnectionString("AzureDatabase"));
+            _businessLogic = new BusinessLogic();
+        }
+
+        [HttpGet("CheckIfUserIsSigned")]
+        public ActionResult CheckIfUserIsSigned(int event_id)
+        {
+            try
+            {
+                _db.Connect();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+            string id = "0";
+            if (Request.Headers.Keys.Contains("Authorization"))
+            {
+                string token = Request.Headers["Authorization"];
+                if (_businessLogic.isJWTValid(token.Replace("Bearer ", "")))
+                {
+                    id = User.Claims.FirstOrDefault(i => i.Type.Contains("UserId")).Value;
+                }
+            }
+            if (id == "0")
+            {
+                return Unauthorized();
+            }
+            try
+            {
+                List<Event> events = new List<Event>();
+                string query = $"SELECT status_id FROM signs_up_for WHERE status_id = 2 AND event_id = {event_id} AND user_id = {id};";
+                var reader = _db.ExecuteQuery(query);
+                if (!reader.HasRows)
+                {
+                    return BadRequest("User isn't signed in any events.");
+                }
+                while (reader.Read())
+                {
+                    if (reader.GetValue(0) != DBNull.Value)
+                    {
+                        events.Add(_businessLogic.FillEvents(reader));
+                    }
+                }
+                reader.Close();
+                _db.Disconnect();
+                return Ok(events.ToList());
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+
+    }
+}
