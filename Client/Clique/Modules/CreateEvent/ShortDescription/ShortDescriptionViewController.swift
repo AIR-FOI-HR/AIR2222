3488e7a9fea84e4de065a6f5263d7aa3fc2ea4ec
Hana Miškić <hmiskic@student.foi.hr>
2022-12-09 12:09:41 +0100

    Adding character countdown in short description

--- Client/Clique/Modules/CreateEvent/ShortDescription/ShortDescriptionViewController.swift
+++ Client/Clique/Modules/CreateEvent/ShortDescription/ShortDescriptionViewController.swift
@@ -12,6 +12,7 @@ class ShortDescriptionViewController: UIViewController {
     
     var returnKeyHandler = IQKeyboardReturnKeyHandler()
     @IBOutlet private var shortDescriptionTextView: UITextView!
+    @IBOutlet private var characterCountLabel: UILabel!
     var createEventObject = CreateEventObject()
     
     override func viewDidLoad() {
@@ -36,14 +37,26 @@ class ShortDescriptionViewController: UIViewController {
         viewContoller.createEventObject = createEventObject
         navigationController?.pushViewController(viewContoller, animated: true)
     }
-    
 }
 extension ShortDescriptionViewController: UITextViewDelegate {
     func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
         let currentText = textView.text ?? ""
         guard let stringRange = Range(range, in: currentText) else { return false }
         let updatedText = currentText.replacingCharacters(in: stringRange, with: text)
+        var countdown = 251 - updatedText.count
+        let text = "Write a short description of your event! U have" + " \(countdown) " + "characters remaining."
+        let range = (text as NSString).range(of: "\(countdown)")
+        let attributedString = NSMutableAttributedString(string:text)
+        if countdown == 0 {
+            attributedString.addAttribute(NSAttributedString.Key.foregroundColor, value: UIColor.systemRed, range: range)
+        } else if countdown > 0 && countdown < 11 {
+            attributedString.addAttribute(NSAttributedString.Key.foregroundColor, value: UIColor.systemYellow , range: range)
+        } else {
+            attributedString.addAttribute(NSAttributedString.Key.foregroundColor, value: UIColor.systemGreen , range: range)
+        }
+        characterCountLabel.attributedText = attributedString
         return updatedText.count <= 250
+        
     }
 }
 
