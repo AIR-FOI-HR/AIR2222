9a49d19abd6739bb722190c728c0bad1c7b68dfb
David-Viljevac <84078493+David-Viljevac@users.noreply.github.com>
2022-12-14 15:18:15 +0100

    Created filters struct to use it for filtering events

---
+++ Client/Clique/Common/Models/Filters.swift
@@ -0,0 +1,14 @@
+//
+//  Filters.swift
+//  Clique
+//
+//  Created by Infinum on 13.12.2022..
+//
+
+import Foundation
+
+struct Filter {
+    var priceMin: Double
+    var priceMax: Double
+    var dateFrom: Date
+}
