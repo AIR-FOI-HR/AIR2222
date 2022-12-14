b695825323ec01e5870cd4ec179889388db695ba
Hana Miškić <hmiskic@student.foi.hr>
2022-12-14 12:03:17 +0100

    CreateEventObject update, removed spaces

--- Client/Clique/Common/Models/CreateEventObject.swift
+++ Client/Clique/Common/Models/CreateEventObject.swift
@@ -8,13 +8,13 @@
 import Foundation
 
 struct CreateEventObject {
-    var eventName : String?
-    var eventLocation : String?
-    var eventTimeStampAPI : String?
-    var eventTimeStampPrint : String?
-    var participantsCount : String?
-    var cost : String?
-    var currency : Currency?
-    var category : Category?
-    var description : String?
+    var eventName: String?
+    var eventLocation: String?
+    var eventTimeStampAPI: String?
+    var eventTimeStampPrint: String?
+    var participantsCount: String?
+    var cost: String?
+    var currency: Currency?
+    var category: Category?
+    var description: String?
 }
