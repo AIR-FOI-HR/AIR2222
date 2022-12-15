7d83784a03c3b2a9bbf5892d30df05c9979e9fb1
David-Viljevac <84078493+David-Viljevac@users.noreply.github.com>
2022-12-15 19:08:41 +0100

    Adding url for api call

--- Client/Clique/Common/Constants/Constants.swift
+++ Client/Clique/Common/Constants/Constants.swift
@@ -8,11 +8,13 @@
 import Foundation
 
 enum Constants {
+
     enum Service {
         private static let baseURL = "https://cliquewebservice20221115180920.azurewebsites.net/api/"
         static let registerURL = baseURL.appending("Authentication/RegisterUser")
         static let loginURL = baseURL.appending("Authentication/LoginUser")
         static let eventsURL = baseURL.appending("Event/GetAllEvents")
+        static let categoriesURL = baseURL.appending("Event/GetCategories")
     }
     
     enum Labels {
